34b965493c9755c1841504739d3a5423824763dd
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2020-02-18 23:45:00 -0300

    Handle extreme left, right bottom and top cases

--- __tests__/gof.js
+++ __tests__/gof.js
@@ -1,37 +1,78 @@
 import { getNeighbors, MAP_DIMENSIONS } from '../src/lib/gof'
 
-it('getNeighbors corner cases', () => {
-  const neighborsLeftBottom = [
-    [0, 1],
-    [1, 1],
-    [1, 0],
-  ]
-  const neighborsLeftTop = [
-    [0, MAP_DIMENSIONS[1] - 1],
-    [1, MAP_DIMENSIONS[1] - 1],
-    [1, MAP_DIMENSIONS[1]],
-  ]
-  const neighborsRightBottom = [
-    [MAP_DIMENSIONS[0], 1],
-    [MAP_DIMENSIONS[0] - 1, 1],
-    [MAP_DIMENSIONS[0] - 1, 0],
-  ]
-  const neighborsRightTop = [
-    [MAP_DIMENSIONS[0] - 1, MAP_DIMENSIONS[1]],
-    [MAP_DIMENSIONS[0] - 1, MAP_DIMENSIONS[1] - 1],
-    [MAP_DIMENSIONS[0], MAP_DIMENSIONS[1] - 1],
-  ]
+describe('getNeighbors', () => {
+  it('corner cases', () => {
+    const neighborsLeftBottom = [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ]
+    const neighborsLeftTop = [
+      [0, MAP_DIMENSIONS[1] - 1],
+      [1, MAP_DIMENSIONS[1] - 1],
+      [1, MAP_DIMENSIONS[1]],
+    ]
+    const neighborsRightBottom = [
+      [MAP_DIMENSIONS[0], 1],
+      [MAP_DIMENSIONS[0] - 1, 1],
+      [MAP_DIMENSIONS[0] - 1, 0],
+    ]
+    const neighborsRightTop = [
+      [MAP_DIMENSIONS[0] - 1, MAP_DIMENSIONS[1]],
+      [MAP_DIMENSIONS[0] - 1, MAP_DIMENSIONS[1] - 1],
+      [MAP_DIMENSIONS[0], MAP_DIMENSIONS[1] - 1],
+    ]
 
-  expect(getNeighbors([0, 0])).toEqual(
-    expect.arrayContaining(neighborsLeftBottom),
-  )
-  expect(getNeighbors([0, MAP_DIMENSIONS[1]])).toEqual(
-    expect.arrayContaining(neighborsLeftTop),
-  )
-  expect(getNeighbors([MAP_DIMENSIONS[0], 0])).toEqual(
-    expect.arrayContaining(neighborsRightBottom),
-  )
-  expect(getNeighbors([MAP_DIMENSIONS[0], MAP_DIMENSIONS[1]])).toEqual(
-    expect.arrayContaining(neighborsRightTop),
-  )
+    expect(getNeighbors([0, 0])).toEqual(
+      expect.arrayContaining(neighborsLeftBottom),
+    )
+    expect(getNeighbors([0, MAP_DIMENSIONS[1]])).toEqual(
+      expect.arrayContaining(neighborsLeftTop),
+    )
+    expect(getNeighbors([MAP_DIMENSIONS[0], 0])).toEqual(
+      expect.arrayContaining(neighborsRightBottom),
+    )
+    expect(getNeighbors([MAP_DIMENSIONS[0], MAP_DIMENSIONS[1]])).toEqual(
+      expect.arrayContaining(neighborsRightTop),
+    )
+  })
+
+  it('extreme left, right, top and bottom', () => {
+    const leftNeighbors = [
+      [0, 1],
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [0, 3],
+    ]
+    const rightNeighbors = [
+      [99, 0],
+      [98, 0],
+      [98, 1],
+      [98, 2],
+      [99, 2],
+    ]
+    const topNeighbors = [
+      [2, 99],
+      [2, 98],
+      [1, 98],
+      [0, 98],
+      [0, 99],
+    ]
+    const bottomNeighbors = [
+      [1, 0],
+      [1, 1],
+      [2, 1],
+      [3, 1],
+      [3, 0],
+    ]
+    expect(getNeighbors([0, 2])).toEqual(expect.arrayContaining(leftNeighbors))
+    expect(getNeighbors([99, 1])).toEqual(
+      expect.arrayContaining(rightNeighbors),
+    )
+    expect(getNeighbors([1, 99])).toEqual(expect.arrayContaining(topNeighbors))
+    expect(getNeighbors([2, 0])).toEqual(
+      expect.arrayContaining(bottomNeighbors),
+    )
+  })
 })
--- src/lib/gof.js
+++ src/lib/gof.js
@@ -2,37 +2,76 @@ const MAP_DIMENSIONS = [99, 99]
 
 const getNeighbors = ([x, y] = []) => {
   // Handle corner cases
-  if (x == 0 || x == MAP_DIMENSIONS[0] || y == 0 || y == MAP_DIMENSIONS[1]) {
-    if (x == 0 && y == 0) {
-      return [
-        [0, 1],
-        [1, 1],
-        [1, 0],
-      ]
-    }
-    if (x == 0 && y == MAP_DIMENSIONS[1]) {
-      return [
-        [0, MAP_DIMENSIONS[1] - 1],
-        [1, MAP_DIMENSIONS[1] - 1],
-        [1, MAP_DIMENSIONS[1]],
-      ]
-    }
-    if (x == MAP_DIMENSIONS[0] && y == 0) {
-      return [
-        [MAP_DIMENSIONS[0] - 1, 0],
-        [MAP_DIMENSIONS[0] - 1, 1],
-        [MAP_DIMENSIONS[0], 1],
-      ]
-    }
-    if (x == MAP_DIMENSIONS[0] && y == MAP_DIMENSIONS[1]) {
-      return [
-        [MAP_DIMENSIONS[0] - 1, MAP_DIMENSIONS[1]],
-        [MAP_DIMENSIONS[0] - 1, MAP_DIMENSIONS[1] - 1],
-        [MAP_DIMENSIONS[0], MAP_DIMENSIONS[1] - 1],
-      ]
-    }
+  if (x == 0 && y == 0) {
+    return [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ]
+  }
+  if (x == 0 && y == MAP_DIMENSIONS[1]) {
+    return [
+      [0, MAP_DIMENSIONS[1] - 1],
+      [1, MAP_DIMENSIONS[1] - 1],
+      [1, MAP_DIMENSIONS[1]],
+    ]
+  }
+  if (x == MAP_DIMENSIONS[0] && y == 0) {
+    return [
+      [MAP_DIMENSIONS[0] - 1, 0],
+      [MAP_DIMENSIONS[0] - 1, 1],
+      [MAP_DIMENSIONS[0], 1],
+    ]
+  }
+  if (x == MAP_DIMENSIONS[0] && y == MAP_DIMENSIONS[1]) {
+    return [
+      [MAP_DIMENSIONS[0] - 1, MAP_DIMENSIONS[1]],
+      [MAP_DIMENSIONS[0] - 1, MAP_DIMENSIONS[1] - 1],
+      [MAP_DIMENSIONS[0], MAP_DIMENSIONS[1] - 1],
+    ]
   }
 
+  // Handle extreme left, right, top and bottom cells
+  // Left
+  if (x == 0) {
+    return [
+      [x, y - 1],
+      [x + 1, y - 1],
+      [x + 1, y],
+      [x + 1, y + 1],
+      [x, y + 1],
+    ]
+  }
+  // Right
+  if (x == MAP_DIMENSIONS[0]) {
+    return [
+      [x, y - 1],
+      [x - 1, y - 1],
+      [x - 1, y],
+      [x - 1, y + 1],
+      [x, y + 1],
+    ]
+  }
+  // Top
+  if (y == MAP_DIMENSIONS[1]) {
+    return [
+      [x + 1, y],
+      [x + 1, y - 1],
+      [x, y - 1],
+      [x - 1, y - 1],
+      [x - 1, y],
+    ]
+  }
+  // Bottom
+  if (y == 0) {
+    return [
+      [x - 1, 0],
+      [x - 1, y + 1],
+      [x, y + 1],
+      [x + 1, y + 1],
+      [x + 1, y],
+    ]
+  }
   return []
 }
 
