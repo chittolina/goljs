ca244a7095dfe834ba9e3c01b23c634e2aeb581e
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2020-02-18 23:15:48 -0300

    Remove unused code

--- __tests__/gof.js
+++ __tests__/gof.js
@@ -1,16 +1,6 @@
 import { getNeighbors, MAP_DIMENSIONS } from '../src/lib/gof'
 
 it('getNeighbors corner cases', () => {
-  // const initialState = []
-
-  // // Initialize an empty matrix, representing the initial state of the game
-  // for (let i = 0; i < MAP_DIMENSIONS[0].length; i++) {
-  //     initialState[j] = []
-  //     for (let j = 0; j < MAP_DIMENSINONS[1].length; j++) {
-  //         initialState[i][j] = 1
-  //     }
-  // }
-
   const neighborsLeftBottom = [
     [0, 1],
     [1, 1],
